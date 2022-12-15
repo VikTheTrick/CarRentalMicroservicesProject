d17ed9179b61209c90034d3b278452ccc005a820
VikTheTrick <viktorvujicic1@gmail.com>
2022-12-15 17:15:11 +0100

    project setup

---
+++ NotificarionService/src/main/java/com/example/notificarionservice/NotificarionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.notificarionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificarionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificarionServiceApplication.class, args);
+    }
+
+}
