63b4dc57d370f3b6672d03a173ec3c3eb6bd3eb7
VikTheTrick <viktorvujicic1@gmail.com>
2022-12-15 17:51:29 +0100

    user service model started

---
+++ UserService/src/main/java/com/example/userservice/domain/User.java
@@ -0,0 +1,23 @@
+package com.example.userservice.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+
+@Entity
+public class User {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+    private String surname;
+
+
+}
---
+++ UserService/src/main/java/com/example/userservice/domain/userTypes/Client.java
@@ -0,0 +1,9 @@
+package com.example.userservice.domain.userTypes;
+
+import com.example.userservice.domain.User;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Client extends User {
+}
